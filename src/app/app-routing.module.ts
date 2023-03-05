a15cd228ef8509f42e81a1ee749eff21014a5114
jcsegurat2309 <juancarlos230999@gmail.com>
2023-03-05 13:48:20 -0600

    primer commit CRUD-Listar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarComponent } from './Persona/agregar/agregar.component';
+import { EditComponent } from './Persona/edit/edit.component';
+import { ListarComponent } from './Persona/listar/listar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Listar', component:ListarComponent},
+  {path: 'Nuevo', component:AgregarComponent},
+  {path: 'edut', component:EditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarComponent } from './Persona/listar/listar.component';
+import { AgregarComponent } from './Persona/agregar/agregar.component';
+import { EditComponent } from './Persona/edit/edit.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarComponent,
+    AgregarComponent,
+    EditComponent,
   ],
   imports: [
     BrowserModule,
