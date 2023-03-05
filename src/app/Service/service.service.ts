160b5cb6e0605a152c02791d087ba5ff64f9823b
jcsegurat2309 <juancarlos230999@gmail.com>
2023-03-05 16:36:43 -0600

    Router

--- src/app/Service/service.service.ts
+++ src/app/Service/service.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Persona } from '../Model/Persona';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  Url= 'http://localhost:8000/Ejemplo1/personas';
+
+  getPersonas(){
+    return this.http.get<Persona[]>(this.Url);
+  }
 }
